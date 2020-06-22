c7ed005d7da12f4a902e423e86cb8a2ce3e609dc
2875823569@qq.com <2875823569@qq.com>
2020-06-22 20:33:23 +0800

    recive_notic

--- 聊天系统/js/Main.js
+++ 聊天系统/js/Main.js
@@ -4,6 +4,11 @@ var sign_str = window.localStorage.getItem('sign_str')
 var username = window.localStorage.getItem('username')
 var nikename = window.localStorage.getItem('nickname')
 
+console.log("id:"+id)
+console.log("sign_str:"+sign_str)
+console.log("username:"+username)
+console.log("nickname:"+nikename)
+
 
 //------------------------------------------------右边隐藏界面--------------------------------------------------------------
 $(".icon-shizi").click(function () {
@@ -34,6 +39,49 @@ $(".add_btn").click(function(){
     })
 })
 
+//------------------------------------------------聊天信息界面--------------------------------------------------------------
+//长轮询
+function longLoop(url,type,data,callback) {
+    $.ajax({
+        url:url,
+        type:type,
+        dataType:"json",
+        timeout:1000*5,
+        data:data,
+        success(data){
+            callback(data)
+        },
+        error(data){
+
+        },
+        complete(){
+            longLoop(url,type,data,callback)
+        }
+    })
+}
+var notic_data = {
+    sign_str:sign_str,
+    user_id:id
+}
+
+longLoop("http://118.24.25.7/chat_api/interface/getMessages.php","GET",notic_data,function (data) {
+    console.log(data)
+    add_information(data.data[0].message_send_time,data.data[0].nickname,data.data[0].head_logo,data.data[0].message)
+})
+function add_information(time,uname,head_logo,message) {
+    $(".notice_area").html(function(n){
+        return $(".notice_area").html()+`<div class="notice_infomation">
+                    <img src="http://118.24.25.7/chat_api${head_logo}" class="notice_uheadimg">
+                    <div class="notice_left">
+                        <div class="notice_information">
+                            <p class="uname">${uname}</p>
+                            <p class="send_time">${time}</p>
+                        </div>
+                        <div class="notice_container">${message}</div>
+                    </div>
+                </div>`
+    })
+}
 
 
 
