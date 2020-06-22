e96f78c69c73e7c085a9eac02c4186e8a134131f
2875823569 <65074797+2875823569@users.noreply.github.com>
2020-06-22 10:15:22 +0800

    Add files via upload
    主文件

---
+++ 聊天系统/js/登陆界面.js
@@ -0,0 +1,86 @@
+$("#loading").click(function () {
+    if($("#uername")[0].value===""){
+        alert("请输入账号")
+        return;
+    }else if($("#password")[0].value===""){
+        alert("请输入密码");
+        return;
+    }
+    var data = {
+        username: `${$("#uername")[0].value}`,
+        password: `${$("#password")[0].value}`
+    }
+    $.ajax({
+        url:"http://118.24.25.7/chat_api/interface/login.php",
+        type:"POST",
+        data:data,
+        // dataType:JSON,
+        success:(function (msg) {
+            if(msg.msg==="success"){
+                location.href = '首页.html'
+
+                var uinformation = [{"id":msg.data.id,"sign_str":msg.data.sign_str,"uname":msg.data.username}]
+                //设置localStorage
+                // console.log(msg)
+                window.localStorage.setItem('id',msg.data.id);
+                window.localStorage.setItem('sign_str',msg.data.sign_str);
+                window.localStorage.setItem('username',msg.data.username);
+                window.localStorage.setItem('nickname',msg.data.nickname);
+                // console.log(msg.data.nickname)
+                alert("登陆成功");
+            }else{
+
+                alert(msg.msg)
+            }
+        }),
+        err:(function () {
+            alert("连接失败")
+        })
+    })
+})
+
+$(".loading:nth-of-type(3)").mousedown(function () {
+    $(".loading:nth-of-type(3)")[0].style.backgroundColor = "rgba(213, 222, 228, 0.51)"
+})
+
+
+$(".register_window .icon-jiantouzuoxi").click(function () {
+    console.log(123)
+    $(".loading_window").toggle("slow")
+    $(".register_window").toggle("slow")
+})
+
+
+$("#register_btn").click(function(){
+    $(".loading_window").toggle("slow")
+    $(".register_window").toggle("slow")
+})
+
+
+//------------------------------------------------注册界面js--------------------------------------------------------------
+$("#regist_btn").click(function () {
+    uname = $("#regist_name input").val()
+    password = $("#regist_password input").val()
+    nickname = $("#regist_nickname input").val()
+    headimg = $("#regist_headimg input").val()
+
+    if(uname===""||password===""||nickname===""){
+        alert("请将信息填完整")
+        return;
+    }
+    if(headimg===""){
+        $.ajax({
+            url:"http://118.24.25.7/chat_api/interface/reg.php",
+            type:"POST",
+            data:{
+                username:uname,
+                password:password,
+                nickname:nickname
+            },
+            success:(function (msg) {
+                // console.log(msg)
+                alert(msg.msg)
+            })
+        })
+    }
+})
---
+++ 聊天系统/js/首页.js
@@ -0,0 +1,40 @@
+//获取传递过来的localStorage
+var id = window.localStorage.getItem('id')
+var sign_str = window.localStorage.getItem('sign_str')
+var username = window.localStorage.getItem('username')
+var nikename = window.localStorage.getItem('nickname')
+
+
+//------------------------------------------------右边隐藏界面--------------------------------------------------------------
+$(".icon-shizi").click(function () {
+    $(".icon-shizi").toggleClass("shizi_isclicked")
+    $(".hidden_rigth").toggleClass("hidden_rigth_display")
+})
+//申请好友
+$("add_btn").click(function(){
+
+    //申请好友
+    $.ajax({
+        url:"http://118.24.25.7/chat_api/interface/addFriend.php",
+        type:"POST",
+        data:{
+            sign_str:sign_str,
+            user_id:id,
+            friend_user_id:$(".add_input").val()
+        },
+        datatype:"JSON",
+        success:(function (msg) {
+            alert(msg.msg)
+        })
+    })
+})
+
+
+
+
+
+//------------------------------------------------个人信息界面--------------------------------------------------------------
+$(".head_img").click(function(){
+    console.log(111)
+})
+
