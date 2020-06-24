45abae408dc94b6b66b9c534f26e10774179fdf5
linlinabc <919904369@qq.com>
2020-06-24 14:36:16 +0800

    friends修改

--- 聊天系统/js/main_box.js
+++ 聊天系统/js/main_box.js
@@ -4,10 +4,14 @@ $(".tomain_window").click(function () {
     $(".main_window")[0].setAttribute("src","Main.html")
     $(".tomain_window i").addClass("isclicked").parent().siblings().children("i").removeClass("isclicked")
 })
+
+
 $(".tofriend_window").click(function () {
-    $(".main_window")[0].setAttribute("src","Main.html")
+    $(".main_window")[0].setAttribute("src","../html/好友列表.html")
     $(".tofriend_window i").addClass("isclicked").parent().siblings().children("i").removeClass("isclicked")
 })
+
+
 $(".toaction").click(function () {
     $(".main_window")[0].setAttribute("src","Main.html")
     $(".toaction i").addClass("isclicked").siblings().parent().siblings().children("i").removeClass("isclicked")
