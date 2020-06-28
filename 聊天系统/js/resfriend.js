d16b1ed03ee57ab9fe7aed8b3b82eca74ff0f3cc
lan <2411762397@qq.com>
2020-06-28 10:53:00 +0800

    提交本地代码并修改最新代码

--- 聊天系统/js/resfriend.js
+++ 聊天系统/js/resfriend.js
@@ -3,10 +3,7 @@ var submitf=document.querySelector(".submitf")
 var regtype=document.querySelector(".regtype")
 console.log(localStorage);
 
-
-
 var leftbtn=document.querySelector(".leftbtn");
-console.log(leftbtn);
 
 leftbtn.onclick=function(){
     // console.log(1);
@@ -32,7 +29,7 @@ function getfriend(){
             console.log(res.data[i]);
             var p = document.createElement('div');
             head_log = res.data[i].head_logo
-            console.log(res.data)
+            // console.log(res.data)
             p.classList.add('friendbox')
             p.innerHTML = `
             <div class="headlogo">
@@ -41,7 +38,7 @@ function getfriend(){
                 <select class="regtype" user_id=${res.data[i].user_id}>
                 <option value="1">同意</option>
                 <option value="2">拒绝</option>
-                <option value="3">拒绝并不再接收此用人好友申请</option>
+                <option value="3">拒绝并不再接收此人好友申请</option>
                 </select>
             <button class="submitf" user_id=${res.data[i].user_id}>提交</button>`
             addfriendbox.append(p)
@@ -60,15 +57,12 @@ function getfriend(){
         console.log(res);
         
     })
-    .always(function(res){
-        if(!res.data.user_id){
-        // getfriend();
-        }
+    .always(function(){
+       
     })
   
 }
 getfriend()
-
 
 
 // 处理好友申请
@@ -83,18 +77,25 @@ function processFriendRequest(obj,val){
         url:"http://118.24.25.7/chat_api/interface/processFriendRequest.php",
         type:"POST",
         data:{
-            sign_strL:localStorage.sign_str,
+            sign_str:localStorage.sign_str,
             user_id:localStorage.id,
             from_user_id:obj.user_id,
             request_id:obj.request_id,
-            process_result:val
+            process_result:val,
         },
-        timeout:3000,
         datatype:"JSON",
     })
-    .done(function(data){
-        console.log(data);
-        
+    .done(function(res){
+        console.log(res);
+        if(res.code==0){
+
+        }else if(res.code==1){
+            console.log(res.data);
+            
+        }else{
+            console.log(11);
+            
+        }
     })
     .fail(function(data){
         console.log(data);
