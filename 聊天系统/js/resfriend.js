079928ae7cdbea113c8fd9a4cc301e92ec73a393
lan <2411762397@qq.com>
2020-06-28 09:34:12 +0800

    提交本地代码并修改最新代码

--- 聊天系统/js/resfriend.js
+++ 聊天系统/js/resfriend.js
@@ -1,15 +1,18 @@
 var addfriendbox=document.querySelector(".addfriendbox");
 var submitf=document.querySelector(".submitf")
 var regtype=document.querySelector(".regtype")
-console.log(regtype);
+console.log(localStorage);
 
 
-// submitf.onclick=function(){
-//     console.log(1);
-    
-// }
 
-// 获取好友申请
+var leftbtn=document.querySelector(".leftbtn");
+console.log(leftbtn);
+
+leftbtn.onclick=function(){
+    console.log(1);
+    location.href='../html/main_box.html'
+}
+
 function getfriend(){
     $.ajax({
         url:"http://118.24.25.7/chat_api/interface/getFriendRequests.php",
@@ -23,23 +26,9 @@ function getfriend(){
     })
     .done(function(res){
         console.log(res);
-        // var datas=JSON.stringify(res.data)
-        // console.log(datas);
-        var arr1=new Array();
-        for(let i=0;i<res.data.length;i++){
-            // regtype.onchange=function(){
-            //     console.log(1);
-                
-            //     // console.log(res.data[i].request_id);
-            //     // arr1.push(res.data[i].request_id)
-            //     // window.localStorage.setItem('fromuserid',arr1)
-            //     // var fromuserid=JSON.stringify(window.localStorage.getItem('fromuserid'))
-            //     // console.log(fromuserid);
-            // }
-        }
-        console.log(localStorage);
         
-        for(i in res.data){
+        
+        for(let i=0;i<res.data.length;i++){
             console.log(res.data[i]);
             var p = document.createElement('div');
             head_log = res.data[i].head_logo
@@ -48,49 +37,68 @@ function getfriend(){
             <div class="headlogo">
             <img src='http://118.24.25.7/${head_log}' class="headimg"></div>
             <div class='nickname'>${res.data[i].nickname}</div>
-            <select class="regtype"><option value="1">同意</option><option value="2">拒绝</option>
-            <option value="3">拒绝并不再接收此用人好友申请</option></select><button class="submitf">提交</button>`
+                <select class="regtype" user_id=${res.data[i].user_id}>
+                <option value="1">同意</option>
+                <option value="2">拒绝</option>
+                <option value="3">拒绝并不再接收此用人好友申请</option>
+                </select>
+            <button class="submitf" user_id=${res.data[i].user_id}>提交</button>`
             addfriendbox.append(p)
+            $(`.submitf[user_id=${res.data[i].user_id}]`).on("click",function(){
+                var processvals=$(`.regtype[user_id=${res.data[i].user_id}]`).val();
+                console.log(localStorage);
+                processFriendRequest(res.data[i],processvals)
+                
+            })
+            
         }
     })
     .fail(function(res){
         console.log(res);
         
     })
-    .always(function(){
+    .always(function(res){
+        if(!res.data.user_id){
         // getfriend();
+        }
     })
+  
 }
 getfriend()
 
 
 
 // 处理好友申请
-// function processFriendRequest(){
-//     $.ajax({
-//         url:"http://118.24.25.7/chat_api/interface/processFriendRequest.php",
-//         type:"POST",
-//         data:{
-//             sign_strL:localStorage.sign_str,
-//             user_id:localStorage.id,
-//             from_user_id:`${res.data.user_id}`,
-//             request_id:`${res.data.request_id}`,
-//             process_result:`${a}`
-//         },
-//         datatype:"JSON",
-//         timeout: 6000
-//     })
-//     .done(function(res){
-//         console.log(res);
+function processFriendRequest(obj,val){
+    console.log(localStorage.sign_str);
+    console.log(localStorage.id);
+    console.log(obj.user_id);
+    console.log(obj.request_id);
+    console.log(val);
+
+    $.ajax({
+        url:"http://118.24.25.7/chat_api/interface/processFriendRequest.php",
+        type:"POST",
+        data:{
+            sign_strL:localStorage.sign_str,
+            user_id:localStorage.id,
+            from_user_id:obj.user_id,
+            request_id:obj.request_id,
+            process_result:val
+        },
+        timeout:3000,
+        datatype:"JSON",
+    })
+    .done(function(data){
+        console.log(data);
         
-//     })
-//     .fail(function(res){
-//         console.log(res);
+    })
+    .fail(function(data){
+        console.log(data);
         
-//     })
-//     .always(function(){
-//         // getfriend();
-//     })
-// }
-// processFriendRequest()
+    })
+    .always(function(){
+    })
+}
+
 
